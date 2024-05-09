38fb8699b54a60a47ccbba20e5f186dfb203003f
Janik Stracke <j.stracke@itsmind.com>
2024-05-09 17:23:01 +0200

    chore: Use npm version of openapitools-generator-cli

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-openapi-generator generate\
-    -i sevdesk-api/openapi.yaml\
-    -g php\
-    -o ./\
-    --package-name itsmind/sevdesk-php-sdk\
-    --additional-properties=disallowAdditionalPropertiesIfNotPresent=false,invokerPackage=Itsmind\\Sevdesk,packageName=itsmind/sevdesk-php-sdk\
-    --git-user-id j-mastr\
-    --git-repo-id sevdesk-php-sdk
+yarn openapi-generator-cli generate\
+          -i sevdesk-api/openapi.yaml\
+          -g php\
+          -o ./\
+          --package-name itsmind/sevdesk-php-sdk\
+          --config config.yaml\
+          --git-user-id j-mastr\
+          --git-repo-id sevdesk-php-sdk
